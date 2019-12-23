ea67d2715ba57c613bd4f61d1a87048c0482c57e
Rodolfo Silva <rasilvap22@gmail.com>
2019-12-22 22:09:01 -0500

    RS: burguer-builder: Added the BuildControl components and Css.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+    //Object.keys spread the keys and transform it to an Array.
     let transformedIngredients = Object.keys( props.ingredients )
-        .map( igKey => {
+        .map( igKey => {//Array javascript object create an Array. -> _ means the element is blank
+            // I care only for the Index.
             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             } );
         } )
         .reduce((arr, el) => {
             return arr.concat(el)
-        }, []);
+        }, []); //Initial value of reduce an empty array.
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients!</p>;
     }
